94de51ac750d51b4a6b786548fe05d0c7329bc79
Michael Hartmeier <mlhartme@github.com>
2013-02-06 09:43:47 +0100

    align package name with maven coordinates

--- src/main/java/net/oneandone/graph/Node.java
+++ src/main/java/net/oneandone/graph/Node.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.oneandone.sushi.graph;
+package net.oneandone.graph;
 
 import java.util.ArrayList;
 import java.util.List;
